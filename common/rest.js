cb499bb7bb98d936e3223c09c2fc0fb59d545aa7
Arain <Arain.Yu@morningstar.com>
2013-08-31 17:51:57 +0800

    Arain - Add some files for admin roles and libs.

---
+++ common/rest.js
@@ -0,0 +1,114 @@
+var responseStatus = {
+	ok: function(res, data) {
+		res.json(200, {
+			statusCode: 200,
+			statusText: 'Ok',
+			data: data || ""
+		});
+	},
+	created: function(res, data) {
+		res.json(201, {
+			statusCode: 201,
+			statusText: 'Created',
+			data: data || ""
+		});
+	},
+	updated: function(res, data) {
+		res.json(204, {
+			statusCode: 204,
+			statusText: 'Updated',
+			data: data || ""
+		});
+	},
+	notFound: function(res) {
+		res.json(404, {
+			statusCode: 404,
+			statusText: 'Not Found!'
+		});
+	},
+	notAcceptable: function(res) {
+		res.json(406, {
+			statusCode: 406,
+			statusText: 'Not Acceptable!'
+		});
+	},
+	internalServerError: function(res) {
+		res.json(500, {
+			statusCode: 500,
+			statusText: 'Internal Server Error!'
+		});
+	},
+	serviceUnavailable: function(res) {
+		res.json(503, {
+			statusCode: 503,
+			statusText: 'Service Unavailable!'
+		});
+	}
+};
+
+var method = {
+	GET: "get",
+	POST: "post",
+	PUT: "put",
+	DELETE: "delete"
+};
+
+var API = {
+	getById: function(req, res) {
+		responseStatus.notFound(res);
+	},
+	getAll: function(req, res) {
+		responseStatus.notFound(res);
+	},
+	search: function(req, res) {
+		responseStatus.notFound(res);
+	},
+	post: function(req, res) {
+		responseStatus.notFound(res);
+	},
+	put: function(req, res) {
+		responseStatus.notFound(res);
+	},
+	delete: function(req, res) {
+		responseStatus.notFound(res);
+	}
+};
+
+var ApiResultModel = function(res, method) {
+	this.type = method || method.GET;
+	this.res = res;
+};
+
+ApiResultModel.prototype.checkAcceptable = function(notAcceptableCondition) {
+	if (notAcceptableCondition) {
+		responseStatus.notAcceptable(this.res);
+		return false;
+	}
+	return true;
+};
+
+ApiResultModel.prototype.responseAPI = function(err, data) {
+	if (err) {
+		responseStatus.internalServerError(this.res);
+		return;
+	}
+
+	switch (this.type) {
+		case method.POST:
+			responseStatus.created(this.res, data);
+			break;
+		case method.PUT:
+			responseStatus.updated(this.res, data);
+			break;
+		case method.DELETE:
+			responseStatus.ok(this.res, data);
+			break;
+		default:
+			responseStatus.ok(this.res, data);
+	}
+};
+
+exports.responseStatus = responseStatus;
+exports.API = API;
+exports.method = method;
+exports.ApiResultModel = ApiResultModel;
---
+++ controllers/admin/role.js
@@ -0,0 +1,46 @@
+var Role = require('../../models').AdminRole;
+var Menu = require('../../models').AdminMenu;
+
+exports.getRoles = function(callback) {
+	Role.find().lean().exec(callback);
+};
+
+exports.getRolesAndMenu = function(callback){
+	Role.find().lean().exec(function(err,roles){
+		if(err){
+			callback(err);
+			return;
+		}else{
+			Menu.find().lean().exec(function(err, menus){
+				var docs = {
+					roles: roles,
+					menus: menus
+				};
+
+				callback(err, docs);
+			});
+		}
+	});
+};
+
+exports.newAndSave = function(id, data, callback) {
+	var roleData = {
+		name: data.name,
+		menus: data.menus || []
+	};
+
+	if (id === null) {
+		var role = new Role(roleData);
+		role.save(callback);
+	} else {
+		Role.update({
+			_id: id
+		}, roleData, callback);
+	}
+};
+
+exports.delete = function(id, callback) {
+	Role.remove({
+		_id: id
+	}, callback);
+};
